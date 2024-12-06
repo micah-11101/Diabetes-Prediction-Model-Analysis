fa35790c4ba541be5122e75d35c398c253347b08
Micah Springer <micahspringer2005@gmail.com>
2024-12-05 20:03:51 -0600

    Added schema for sql table

---
+++ diabetestableschema.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS diabetes;
+
+CREATE TABLE diabetes (
+	age FLOAT,
+	sex FLOAT,
+	highchol FLOAT,
+	cholcheck FLOAT,
+	bmi FLOAT,
+	smoker FLOAT,
+	heartdiseaseorattack FLOAT,
+	physactivity FLOAT,
+	fruits FLOAT,
+	veggies FLOAT,
+	hvyalcohol FLOAT,
+	genhlth FLOAT,
+	menthlth FLOAT,
+	physhlth FLOAT,
+	diffwalk FLOAT,
+	stroke FLOAT,
+	highbp FLOAT,
+	diabetes FLOAT
+	
+
+);
+
+SELECT * FROM diabetes
